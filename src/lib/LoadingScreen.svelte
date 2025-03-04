b3dc6646a54a580e4cf9a1986b0a1a39d97d5d16
technolab132 <krishnawankhede2020@gmail.com>
2025-03-04 11:29:22 +0530

    asddaasd

--- src/lib/LoadingScreen.svelte
+++ src/lib/LoadingScreen.svelte
@@ -14,7 +14,7 @@
 {#if visible}
     <div 
         transition:fade={{duration: 500}}
-        class="fixed inset-0 bg-gradient-to-r from-black m-2 rounded-xl via-blue-950 to-black z-50 flex flex-col text-gray-500 items-center justify-center animate-zoom"
+        class="fixed inset-0 bg-gradient-to-r from-black  rounded-xl via-blue-950 to-black z-50 flex flex-col text-gray-500 items-center justify-center animate-zoom"
     >
         <div class="animate-pulse">
             <img src="/Logo%20final/Logo-with-crown.png" alt="Logo" class="w-1/2 mx-auto object-contain" />
